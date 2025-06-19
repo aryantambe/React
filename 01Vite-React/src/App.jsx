fcfedf5afdd71432eeabeed381b58e5d83f13f44
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-19 21:09:35 +0530

    Testing some features of react as well as using fragments

--- 01Vite-React/src/App.jsx
+++ 01Vite-React/src/App.jsx
@@ -1,6 +1,10 @@
+import Test from "./test"
+
 function App() {
-  return (
+  return (<>  {/*this empty tag is a fragment, also this is the syntax for a comment in jsx file*/} 
    <h1>React using vite</h1>
+   <Test/>
+   </>
   )
 }
 export default App
---
+++ 01Vite-React/src/Test.jsx
@@ -0,0 +1,6 @@
+function Test(){
+    return(<><p>This is a new file called test.jsx and i am writing this paragraph to test thing</p>
+     <h5>Meow</h5></> )
+    
+}
+export default Test;
