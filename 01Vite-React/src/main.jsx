a6171782f3a60b7d95b7dc4e57b37545ebd3c84d
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-20 15:59:31 +0530

    Created a react element using React.createElement

--- 01Vite-React/src/main.jsx
+++ 01Vite-React/src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+
+const reactElement=React.createElement(
+  'a',
+  {
+    href:'https://google.com',
+    target:'_blank'
+  },
+  'Click me to visit google'
+)
 const root=createRoot(document.getElementById('root')); // Creates a react root (The starting point of the react app)
 root.render(  //Fills the root with the App component we made
   <StrictMode>
     <App />
+    {reactElement}
   </StrictMode>, //The strict mode warns us about any potential problems (used at the time of development)
 )
