573faa9dc441efe8d2618e1aa00a3a96c8ca3635
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-20 16:32:46 +0530

    Injecting a js variable in jsx

--- 01Vite-React/src/App.jsx
+++ 01Vite-React/src/App.jsx
@@ -1,8 +1,11 @@
 import Test from "./test"
 
 function App() {
+
+  const username="Aryan"
   return (<>  {/*this empty tag is a fragment, also this is the syntax for a comment in jsx file*/} 
    <h1>React using vite</h1>
+   <h1>Hello my name is {username}</h1>
    <Test/>
    </>
   )
