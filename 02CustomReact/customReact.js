2b5adb7002be045e5db80622b4af1776a50c3b71
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-20 15:25:43 +0530

    Used for in loop to increase code efficiency for custom react

--- 02CustomReact/customReact.js
+++ 02CustomReact/customReact.js
@@ -9,12 +9,20 @@ const reactElement={
 const root=document.querySelector('#root');
 
 function customRender(reactElement,root){
+    // const domElement=document.createElement(reactElement.type)
+    // domElement.innerHTML=reactElement.children;
+    // domElement.setAttribute('href',reactElement.props.href);
+    // domElement.setAttribute('target',reactElement.props.target);
+
+    // root.appendChild(domElement);
+
     const domElement=document.createElement(reactElement.type)
     domElement.innerHTML=reactElement.children;
-    domElement.setAttribute('href',reactElement.props.href);
-    domElement.setAttribute('target',reactElement.props.target);
-
-    root.appendChild(domElement);
+    for (const prop in reactElement.props) {
+        if(prop==='children') continue;
+        domElement.setAttribute(prop,reactElement.props[prop]);
+    }
+    root.appendChild(domElement)
 
 }
 customRender(reactElement,root);
