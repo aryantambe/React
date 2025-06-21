8ae6a69434423ce5ee9a392174915e98158d5c38
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-21 18:02:36 +0530

    Project completed using useState hooks

--- 03Counter/src/App.jsx
+++ 03Counter/src/App.jsx
@@ -4,15 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter=5
+  //let counter=5  This variable will not be propogated in the UI so we use hooks (useState)
+
+  const [counter,setCounter]=useState(0)
+  const increaseValue=()=>{
+    if(counter==20){
+      alert("Max value reached bro");
+    }
+    else{
+    setCounter(counter+1);
+
+    }
+  }
+  const decreaseValue=()=>{
+    if(counter==0) {
+      alert("Counter is 0 dude");
+    }
+    else{
+    setCounter(counter-1);
+    }
+  }
 
   return (
     <>
     <h1>React Project #2</h1>
     <h2>Counter Value: {counter}</h2>
-    <button onClick={(evt)=>{alert("button clicked");alert(evt.type)}}>Increase Value</button>
+    <button onClick={increaseValue} >Increase Value</button>
     <br/>
-    <button>Decrease Value</button>
+    <button onClick={decreaseValue}>Decrease Value</button>
+    <p>This is a footer: {counter}</p>
     </>
   )
 }
