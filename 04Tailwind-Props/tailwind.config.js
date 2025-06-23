57f39e1a1ffeefbb92c6e08f4f4109897672ce8e
Aryan <aryan.tambe04@gmail.com>
2025-06-23 21:05:00 +0530

    Manually created these files, due to npx error

---
+++ 04Tailwind-Props/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this depending on your setup
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
