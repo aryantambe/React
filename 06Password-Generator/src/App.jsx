c3a389da923a5d3c567f28be973e3e0b314b0a24
Aryan <aryan.tambe04@gmail.com>
2025-06-25 02:31:03 +0530

    changes

--- 06Password-Generator/src/App.jsx
+++ 06Password-Generator/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length,setLength]=useState(8);
+  const [numAllowed,setNumAllowed]=useState(false);
+  const [charAllowed,setCharAllowed]=useState(false);
+  const [password,setPassword]=useState("");
+
+  const passwordGenerator=()=>{}
 
   return (
     <>
-      <h1 className='bg-amber-500'>Hey</h1>
+      <h1 className='text-6xl text-center text-white mt-4  '>Password-Generator</h1>
     </>
   )
 }
