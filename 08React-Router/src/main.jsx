71a570ff88a49eb4cec4deabbfc28db0c2377c1e
Aryan <aryan.tambe04@gmail.com>
2025-06-29 01:03:04 +0530

    Using createRoutesFromElements to define routes

--- 08React-Router/src/main.jsx
+++ 08React-Router/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 
 const router=createBrowserRouter([
@@ -20,10 +21,27 @@ const router=createBrowserRouter([
       {
         path:"about",
         element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
       }
     ]
   }
-])
+]);
+
+//Another way to define routes
+
+// const router=createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout/>}>
+//       <Route path='' element={<Home/>}>
+//        ....
+//      <Route/>
+//   )
+// )
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
