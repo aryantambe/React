5cf7c8e96a34c6592aa745c4cf2e8315ecb14652
Aryan <aryan.tambe04@gmail.com>
2025-07-04 23:55:59 +0530

    Created context and showcased it using this login page mini project

--- 09Context-API/src/Context/UserContextProvider.jsx
+++ 09Context-API/src/Context/UserContextProvider.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider=({children})=>{
-    const [user,setUser]=useState(null);
+const UserContextProvider=({children})=>{ //children is the special prop
+    const [user,setUser]=useState(null); //defining a  state variable
     return(
-        <UserContext.Provider value={{user,setUser}}>
-        {children}
+        <UserContext.Provider value={{user,setUser}}> {/*giving value to the context i created earlier*/}
+        {children} {/*children is any component*/}
         </UserContext.Provider>
     )
 }
 
 export default UserContextProvider
+
+// Thing	                                     What it means
+// children	                             Whatever component is inside UserContextProvider
+// UserContext.Provider	                The actual source of the data for the context
+// value={{ user, setUser }}	        This is the data you’re sharing globally
+// useContext(UserContext)	            How components get that shared data
