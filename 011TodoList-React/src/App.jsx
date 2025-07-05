995d3801243063f64e22c0fcfa9cc1d178d8d9e9
Aryan <aryan.tambe04@gmail.com>
2025-07-05 23:50:19 +0530

    mimi as background

--- 011TodoList-React/src/App.jsx
+++ 011TodoList-React/src/App.jsx
@@ -48,28 +48,35 @@ function App() {
   }, [todos]);
 
   return (
-    <>
-      <TodoProvider
-        value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+  <>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div
+        className="bg-cover bg-center min-h-screen py-8"
+        style={{
+          backgroundImage:/* some cute cat pics - https://images.pexels.com/photos/10954832/pexels-photo-10954832.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260       https://images.pexels.com/photos/4701546/pexels-photo-4701546.jpeg?_gl=1*16pu4sd*_ga*MTMwNzI0NDMzNC4xNzUwOTM5NTM4*_ga_8JE65Q40S6*czE3NTE3Mzg5NTckbzIkZzEkdDE3NTE3MzkxODMkajI2JGwwJGgw*/ 
+             "url('/mimi1.jpg')",
+        }}
       >
-        <div className="bg-[#172842] min-h-screen py-8">
-          <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-            <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-              ❤️ What's on your mind Divya? ❤️
-            </h1>
-            <div className="mb-4">
-              <TodoForm />
-            </div>
-            {todos.map((todo) => (
-              <div className="w-full" key={todo.id}>
-                <TodoItem todo={todo} />
-              </div>
-            ))}
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white bg-black/60 backdrop-blur-md">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            What's on your mind Divya? ❤️
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
           </div>
+          {todos.map((todo) => (
+            <div className="w-full" key={todo.id}>
+              <TodoItem todo={todo} />
+            </div>
+          ))}
         </div>
-      </TodoProvider>
-    </>
-  );
+      </div>
+    </TodoProvider>
+  </>
+);
+
 }
 
 export default App;
