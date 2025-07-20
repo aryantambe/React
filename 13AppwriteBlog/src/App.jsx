a6a334259c33af7eddc248ea19c066ae88a7b464
Aryan <aryan.tambe04@gmail.com>
2025-07-20 15:37:15 +0530

    removed direct importing of .env variables, instead exported them in config.js

--- 13AppwriteBlog/src/App.jsx
+++ 13AppwriteBlog/src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
  
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL); {/*This is how you access enviornment variables in react using vite*/}
-
   return (
     <>
       <h1>A blog app with appwrite</h1>
